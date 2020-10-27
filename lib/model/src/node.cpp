71fbec8d555463751db2e363a524113f89374600
Ciprian Mehedincu <mehe3.14@gmail.com>
2020-10-27 09:43:00 +0100

    Changed port_ptr_t to std::unique_ptr.

--- lib/model/src/node.cpp
+++ lib/model/src/node.cpp
@@ -9,8 +9,8 @@ connection_state node::connect(std::uint64_t this_out, const std::unique_ptr<nod
 	if (state != connection_state::VALID)
 		return state;
 
-	const auto this_port = _out_ports[this_out];
-	const auto other_port = other->_in_ports[other_in];
+	const auto this_port = _out_ports[this_out].get();
+	const auto other_port = other->_in_ports[other_in].get();
 
 	if (other_port->is_connected())
 		return connection_state::ERR_IN_PORT_CONNECTED;
@@ -28,8 +28,8 @@ connection_state node::disconnect(std::uint64_t this_out, const std::unique_ptr<
 	if (state != connection_state::VALID)
 		return state;
 
-	const auto this_port = _out_ports[this_out];
-	const auto other_port = other->_in_ports[other_in];
+	const auto this_port = _out_ports[this_out].get();
+	const auto other_port = other->_in_ports[other_in].get();
 
 	const auto& it = _connections.find(this_port);
 	if (it == _connections.end())
@@ -56,8 +56,8 @@ connection_state node::validate(std::uint64_t this_out, const std::unique_ptr<no
 	if (other_in >= other->_in_ports.size())
 		return connection_state::ERR_IN_PORT_INVALID;
 
-	const auto this_port = _out_ports[this_out];
-	const auto other_port = other->_in_ports[other_in];
+	const auto this_port = _out_ports[this_out].get();
+	const auto other_port = other->_in_ports[other_in].get();
 
 	if (this_port->get_dir() == other_port->get_dir())
 		return connection_state::ERR_SAME_DIRECTION;
