91685e0d6b2e007cfcf7b42174ed9de0c1726249
mehe_linux <mehe>
2022-09-10 17:40:43 +0200

    Move model building logic to library class.

--- lib/controller/header/library.hpp
+++ lib/controller/header/library.hpp
@@ -5,6 +5,7 @@
 #include <memory>
 #include <string>
 #include <unordered_map>
+#include <unordered_set>
 #include <vector>
 
 namespace mbd
@@ -17,13 +18,13 @@ public:
 
   /* Registers a model builder with the same name as the model.*/
   template <typename M, typename... Args>
-  inline void register_model(const std::string &name, Args &&...args);
+  constexpr inline void register_model(const std::string &name, Args &&...args);
 
   /* Calls a model builder with the same name as the model.*/
   model::ptr_t build_model(const std::string &name) const;
 
   /* Returns a list of all the models that can be built.*/
-  const std::vector<std::string> &get_model_names() const;
+  const std::unordered_set<std::string> &get_model_names() const;
 
   /* Returns the name of the lib.*/
   const std::string &get_name() const;
@@ -32,21 +33,22 @@ private:
   using factory_t = std::function<model::ptr_t()>;
 
   std::unordered_map<std::string, factory_t> _factories;
-  std::vector<std::string> _model_names;
-  std::string _name;
+  std::unordered_set<std::string> _model_names;
+  std::string _lib_name;
 };
 
 template <typename M, typename... Args>
-inline void lib::register_model(const std::string &name, Args &&...args)
+constexpr inline void lib::register_model(const std::string &name,
+                                          Args &&...args)
 {
   if (_factories.find(name) != _factories.end())
     return;
 
-  const factory_t &factory = [&]() {
-    return std::make_unique<M>(name, std::forward<Args>(args)...);
+  const auto &factory = [=]() {
+    return std::make_unique<M>(name, std::move(args)...);
   };
 
   _factories[name] = std::move(factory);
-  _model_names.push_back(name);
+  _model_names.insert(name);
 }
 } // namespace mbd
--- lib/controller/src/library.cpp
+++ lib/controller/src/library.cpp
@@ -1,14 +1,15 @@
 #include "library.hpp"
 #include "model.hpp"
+#include <unordered_set>
 
 namespace mbd
 {
 
-lib::lib(const std::string &name) : _name(name) {}
+lib::lib(const std::string &name) : _lib_name(name) {}
 
 model::ptr_t lib::build_model(const std::string &name) const
 {
-  const auto &it = _factories.find(name);
+  const auto it = _factories.find(name);
   if (it != _factories.end())
   {
     const auto &factory = it->second;
@@ -18,11 +19,11 @@ model::ptr_t lib::build_model(const std::string &name) const
   return nullptr;
 }
 
-const std::vector<std::string> &lib::get_model_names() const
+const std::unordered_set<std::string> &lib::get_model_names() const
 {
   return _model_names;
 }
 
-const std::string &lib::get_name() const { return _name; }
+const std::string &lib::get_name() const { return _lib_name; }
 
 } // namespace mbd
