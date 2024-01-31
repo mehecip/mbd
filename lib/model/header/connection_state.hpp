9498b938564c92be0546eb3d138343077bbd6545
mehe <mehe@mehe>
2024-01-31 18:33:31 +0100

    Remove unused enums.

--- lib/model/header/connection_state.hpp
+++ lib/model/header/connection_state.hpp
@@ -9,40 +9,32 @@ namespace mbd {
 
 enum class connection_state : std::uint8_t {
   VALID = 0,
-  INVALID,
   CONNECTED,
   NOT_CONNECTED,
   ERR_SAME_DIRECTION,
   ERR_DIFF_TYPE,
-  ERR_UNKNOWN,
   ERR_IN_PORT_INVALID,
   ERR_OUT_PORT_INVALID,
   ERR_IN_PORT_CONNECTED,
-  ERR_PORT_NOT_CONNECTED,
   ERR_SAME_MODEL
-
 };
 
 const inline std::string connection_info(connection_state state) {
   const std::unordered_map<connection_state, std::string, enum_class_hash>
       info = {
           {connection_state::VALID, "Valid connection"},
-          {connection_state::INVALID, "Invalid connection"},
           {connection_state::CONNECTED, "Connection established"},
           {connection_state::NOT_CONNECTED, "Not connected"},
           {connection_state::ERR_SAME_MODEL,
            "Error cannot connect ports on the same model."},
           {connection_state::ERR_SAME_DIRECTION, "Error same port direction"},
           {connection_state::ERR_DIFF_TYPE, "Error ports have different types"},
-          {connection_state::ERR_UNKNOWN, "Error unknown"},
           {connection_state::ERR_IN_PORT_INVALID,
            "Error input port index is invalid"},
           {connection_state::ERR_OUT_PORT_INVALID,
            "Error output port index is invalid"},
           {connection_state::ERR_IN_PORT_CONNECTED,
-           "Error input port is already connected"},
-          {connection_state::ERR_PORT_NOT_CONNECTED,
-           "Error ouput port had no connections"}};
+           "Error input port is already connected"}};
 
   return info.at(state);
 }
