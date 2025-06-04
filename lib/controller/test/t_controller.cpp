b5fe6829112b6086be886766833ac66a8e14c51a
mehe <mehe>
2025-06-04 14:35:41 +0200

    Add disconnect and multiple srcs tests.

--- lib/controller/test/t_controller.cpp
+++ lib/controller/test/t_controller.cpp
@@ -51,26 +51,22 @@ private:
 };
 
 const auto &check_execution_order =
-    [](const auto &actual,
-       const std::vector<std::vector<std::string>> &expected) {
+    [](const auto &actual_models,
+       const std::vector<std::vector<std::string>> &expected_names) {
 
-      ASSERT_EQ(actual.size(), expected.size());
-
-      std::size_t idx = 0;
-      for (auto models : actual) {
-
-        EXPECT_EQ(models.size(), expected[idx].size())
-            << "The number of models in priority vector " << idx
-            << " does not match the expected number.";
-
-        for (std::size_t i = 0; i < models.size(); ++i)
+      std::vector<std::vector<std::string>> actual_names;
+      for (const auto &models : actual_models)
+      {
+        std::vector<std::string> names;
+        for (const auto &model : models)
         {
-          EXPECT_EQ(models[i]->get_name(), expected[idx][i])
-              << "Model at index " << i << " in priority vector " << idx
-              << " does not match the expected model.";
+          names.push_back(model->get_name());
         }
-        ++idx;
+        std::sort(names.begin(), names.end());
+        actual_names.push_back(names);
       }
+      
+      EXPECT_EQ(actual_names, expected_names);
     };
 
 TEST(ControllerTest, tConnect)
@@ -118,9 +114,87 @@ TEST(ControllerTest, tExecutionOrder)
   auto order = fx._ctrl.execution_order();
   check_execution_order(order, {
     {"constant source"},
-    {"gain"}, {"sink"}
+    {"gain"},
+    {"sink"}
   });
 
+  fx._ctrl.disconnect("constant source", 0, "gain", 0);
+
+  // the source is not connected to anything, so it should be the only model to be actually executed
+  order = fx._ctrl.execution_order();
+  check_execution_order(order, {
+    {"constant source"}
+  });
+
+  // now connect the source to the gain again
+  fx._ctrl.connect("constant source", 0, "gain", 0);
+  EXPECT_EQ(flag, true) << "Re-Connect should work.";
+
+  order = fx._ctrl.execution_order();
+  check_execution_order(order, {
+    {"constant source"},
+    {"gain"}
+  });
+
+  // now connect the gain to the sink again
+  flag = fx._ctrl.connect("gain", 0, "sink", 0);
+  EXPECT_EQ(flag, true) << "Re-Connect should work.";
+
+  order = fx._ctrl.execution_order();
+  check_execution_order(order, {
+    {"constant source"},
+    {"gain"},
+    {"sink"}
+  });
+}
+
+
+TEST(ControllerTest, tExecutionOrder_MultipleSources)
+{
+  auto lib = get_math_lib<double>("double");
+
+  ControllerFixture fx;
+  fx.addModels(lib, {"const_src", "gain", "sink", "lin_src", "double_to", "sum", "delay"});
+
+  auto flag = fx._ctrl.connect("constant source", 0, "add", 0);
+  EXPECT_EQ(flag, true) << "Connect should work.";
+
+  flag = fx._ctrl.connect("linear source", 0, "type convertor", 0);
+  EXPECT_EQ(flag, true) << "Connect should work.";
+
+  flag = fx._ctrl.connect("type convertor", 0, "add", 1);
+  EXPECT_EQ(flag, true) << "Connect should work.";
+
+  flag = fx._ctrl.connect("add", 0, "unit delay", 0);
+  EXPECT_EQ(flag, true) << "Connect should work.";
+
+  flag = fx._ctrl.connect("unit delay", 0, "gain", 0);
+  EXPECT_EQ(flag, true) << "Connect should work.";
+
+  flag = fx._ctrl.connect("gain", 0, "sink", 0);
+  EXPECT_EQ(flag, true) << "Connect should work.";
+
+  auto order = fx._ctrl.execution_order();
+  check_execution_order(order, {
+    {"constant source", "linear source", "unit delay"},
+    {"type convertor" },
+    {"add"},
+    {"gain"},
+    {"sink"}
+  });
+}
+
+TEST(ControllerTest, tExecutionOrder_UnconnectedSources)
+{
+  auto lib = get_math_lib<double>("double");
+
+  ControllerFixture fx;
+  fx.addAllModels(lib);
+
+  auto order = fx._ctrl.execution_order();
+  check_execution_order(order, {
+    {"constant source", "linear source", "unit delay"}
+  });
 }
 
 TEST(ControllerTest, tRun)
@@ -167,6 +241,31 @@ TEST(ControllerTest, tRun)
 
   std::vector<int> exp {0, 0, -15, -15, -15, 30};
 
-  const auto& actual = sink->read();
+  auto actual = sink->read();
   EXPECT_EQ(actual, exp) << "Sink should contain the expected values after running the controller.";
+
+  fx._ctrl.disconnect("constant source", 0, "gain", 0);
+  fx._ctrl.run(2);
+
+  actual = sink->read();
+  EXPECT_EQ(actual, exp) << "Sink should not have any other values since the source is disconnected.";
+
+  // reconnect the source and run the controller again
+  flag = fx._ctrl.connect("constant source", 0, "gain", 0);
+  EXPECT_EQ(flag, true) << "Connect should work.";
+  fx._ctrl.run(2);
+
+  actual = sink->read();
+  EXPECT_EQ(actual, exp) << "Sink should not have any other values since the gain is disconnected.";
+
+  // reconnect the gain to sink and run the controller again
+  flag = fx._ctrl.connect("gain", 0, "sink", 0);
+  EXPECT_EQ(flag, true) << "Connect should work.";
+
+  fx._ctrl.run(2);
+
+  exp = {0, 0, -15, -15, -15, 30, 30, 30};
+  actual = sink->read();
+  EXPECT_EQ(actual, exp) << "Sink should contain the expected values after reconnecting the source and running the controller again.";
+
 }
